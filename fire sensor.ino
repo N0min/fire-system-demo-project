58409af1c29183f0fa4574d2c521fe24d27bdf3c
N0min <109933862+N0min@users.noreply.github.com>
2022-10-24 11:14:13 +1000

    Update fire sensor.ino

--- fire sensor.ino
+++ fire sensor.ino
@@ -1,10 +1,10 @@
-//#include <SoftwareSerial.h>
+
 #include "DHT.h"
-//SoftwareSerial Bluetooth(8,7);//RX and TX
+
 const int buzzer = 0; //buzzer to arduino pin 9
 int flame_sensor = 2; //flame sensor pin
 int flame_detected;
-String buzz = "off";
+String buzz = "off";    //default buzzer mode
 String fs = "on";
 int alarm = 0; // default fire alarm mode
 #define DHTPIN 21      // Digital pin connected to the DHT sensor
@@ -16,7 +16,7 @@ void setup(){
     pinMode(DHTPIN, INPUT);
     dht.begin();
   Serial.begin(9600);
-  //Bluetooth.begin(9600);
+
   pinMode(buzzer, OUTPUT); // Set buzzer - pin 0 as an output
   Serial1.begin(9600); // BT def communication
   pinMode(flame_sensor, INPUT);
@@ -27,7 +27,7 @@ void loop(){
 
    String state=Serial1.readString();
    
-    if (state == "on"){
+    if (state == "on"){                 //check state for system. it's controling purpose. 
      fs = state;
     Serial.println("fire system is turned on");
     }
@@ -36,42 +36,42 @@ void loop(){
       Serial.println("fire system is turned off");
      }
    }
-   if(fs == "on"){
+   if(fs == "on"){                          //fire system working mode. 
     float h = dht.readHumidity();
     float t = dht.readTemperature();
     float f = dht.readTemperature(true);
     float hif = dht.computeHeatIndex(f, h);
     float hic = dht.computeHeatIndex(t, h, false);  
 
-    Serial1.print(F(" Humidity: "));
+    Serial1.print(F(" Humidity: "));            // send  humi data to serial port
     Serial1.print(h);
-    Serial1.print(F("%  Temperature: "));
+    Serial1.print(F("%  Temperature: "));      // send  temp data to serial port
     Serial1.print(t);
     Serial1.print(F("C "));
-    Serial1.print(F("fire: "));
-    Serial1.print(alarm);
+    Serial1.print(F("fire: "));         //send fire data if 0-no detect 1-fire detected to serial port
+    Serial1.print(alarm);               //send fire notfication data serial port
     Serial1.println(F("F"));
-    delay(5000);
+    delay(5000);                    
     
-      flame_detected = digitalRead(flame_sensor);
+      flame_detected = digitalRead(flame_sensor);       // scan flame sensor if flame detect send alarm to serial port
       if (flame_detected == 1)
       {
         alarm = 1;
-        digitalWrite(buzzer, HIGH);     //alarming 
-        delay(2000);
+        digitalWrite(buzzer, HIGH);     //alarming for buzzer 
+        delay(2000);                
         digitalWrite(buzzer, LOW);
         delay(2);
            
       }
       else
       {
-        digitalWrite(buzzer, LOW);
+        digitalWrite(buzzer, LOW);              // delaynig alarm
         alarm = 0;
       }
     
     delay(1000);
    }
-   else if(fs == "off"){
+   else if(fs == "off"){                        //when fire detect system turned off. it will only send temp and humi data to serial port
     float h = dht.readHumidity();
     float t = dht.readTemperature();
     float f = dht.readTemperature(true);
